3fbcf2a56ca45e489a3a62b9b0b1d221072c0d8b
vivek-wo <vivek.wei.wu@gmail.com>
2019-05-09 00:08:43 +0800

    重构蓝牙结构

--- ble/src/main/java/com/vivek/wo/ble/BluetoothCommObserver.java
+++ ble/src/main/java/com/vivek/wo/ble/BluetoothCommObserver.java
@@ -2,9 +2,22 @@ package com.vivek.wo.ble;
 
 public interface BluetoothCommObserver {
 
+    /**
+     * 连接完成
+     */
     void connectComplete();
 
+    /**
+     * 连接断开
+     *
+     * @param throwable
+     */
     void connectLost(Throwable throwable);
 
+    /**
+     * 远程数据监听
+     *
+     * @param data
+     */
     void remoteDataChanged(byte[] data);
 }
